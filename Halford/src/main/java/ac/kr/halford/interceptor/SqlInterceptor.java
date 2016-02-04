b09da07914f5e88102def7cc58e797491353aef5
USER <USER@USER-PC>
2016-02-04 17:03:08 +0900

    attributes removing function(proceeding)

--- Halford/src/main/java/ac/kr/halford/interceptor/SqlInterceptor.java
+++ Halford/src/main/java/ac/kr/halford/interceptor/SqlInterceptor.java
@@ -1,6 +1,7 @@
 package ac.kr.halford.interceptor;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
@@ -34,41 +35,25 @@ public class SqlInterceptor implements Interceptor {
 		MappedStatement ms       = (MappedStatement)args[0];
 		Object          param    = (Object)args[1];
 		BoundSql        boundSql = ms.getBoundSql(param);
-		String sql = getFullSqlQuery(boundSql, param);
+		String dq = getFullSqlQuery(boundSql, param);
 		
-		System.out.println("====================================");
-		System.out.println(invocation.getMethod().getName());
-		System.out.println(ms.getId());
-		System.out.println(boundSql.getSql());
-		System.out.println(param);
-		System.out.println(sql);
-		System.out.println("====================================");
+		if (removeAttributes(boundSql.getSql(), dq)) return invocation.proceed();  
+		else return new ArrayList<Object>();
 		
-//		Object[] args = invocation.getArgs();
-//		MappedStatement ms = (MappedStatement) args[0];
-//		Map<String, String> params = (Map<String, String>)args[1];
-//		BoundSql boundSql = ms.getBoundSql(params);
-//		StringBuilder sql = new StringBuilder(boundSql.getSql());
-//		
-//		for (ParameterMapping param : boundSql.getParameterMappings()) {
-//			String property = param.getProperty();
-//			int index = sql.indexOf("?");
-//			sql.replace(index, index + 1, "'" + params.get(property) + "'");
-//		}
-//		
-//		Object proceed = invocation.proceed();
-//		
-//		logger.info("{} ==>  Preparing: {}", ms.getId(), sql.toString());
-//		if (proceed != null && proceed instanceof List) {
-//			List<Map<String, String>> list = (List<Map<String, String>>) proceed;
-//			if (list.size() > 0) {
-//				logger.info("{} ==>     Result: {}", ms.getId(), list.get(0));
-//			}
-//			
-//			logger.info("{} <==      Total: {}", ms.getId(), list.size());
-//		}
+//		System.out.println("====================================");
+//		System.out.println(invocation.getMethod().getName());
+//		System.out.println(ms.getId());
+//		System.out.println(boundSql.getSql());
+//		System.out.println(param);
+//		System.out.println(sql);
+//		System.out.println("====================================");
 		
-		return invocation.proceed();
+		
+		// if the sql is normal, return invocation.proceed.
+		
+		
+		// if function detect that the sql is abnormal, return empty ArrayList.
+//		return new ArrayList<Object>();
 	}
 
 	@Override
@@ -85,20 +70,25 @@ public class SqlInterceptor implements Interceptor {
 	private String getFullSqlQuery (BoundSql boundSql, Object param) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
 		String sql = boundSql.getSql();
 		
+		// 현재 모델 클래스나 맵 형식의 매개변수를 이용하기 때문에 이상이 없음.
+		// 정수 하나의 경우 모든 sql 사용자 입력을 대입하는 루틴은 구현 x
 		if (param != null) {
 			if (param instanceof String) sql = sql.replaceFirst("\\?", "'"+param.toString()+"'");
 			else if (param instanceof Integer || param instanceof Long || param instanceof Float || param instanceof Double) sql = sql.replaceFirst("\\?", param.toString());
 			else if (param instanceof Map) {
 				List<ParameterMapping> parameterMappingList = boundSql.getParameterMappings();
+				
 				for (ParameterMapping parameterMapping : parameterMappingList) {
 					String propertyValue = parameterMapping.getProperty();
 					Object value = ((Map)param).get(propertyValue);
+					
 					if (value instanceof String) sql = sql.replaceFirst("\\?", "'"+value.toString()+"'");
-					else sql = sql.replaceFirst("\\?", param.toString());
+					else sql = sql.replaceFirst("\\?", value.toString());
 				}
 			} else {
 				List<ParameterMapping> parameterMappingList = boundSql.getParameterMappings();
 				Class<? extends Object> paramClass = param.getClass();
+				
 				for (ParameterMapping parameterMapping : parameterMappingList) {
 					String propertyValue = parameterMapping.getProperty();
 					Field field = paramClass.getDeclaredField(propertyValue);
@@ -112,6 +102,55 @@ public class SqlInterceptor implements Interceptor {
 		}
 		
 		return sql;
+	}
+	
+	private boolean removeAttributes(String FQ, String DQ) {
+		
+		String stringReg = "=\\s*'([^']+)'";
+//		String stringReg = "=\\s*'.*'";
+		String numberReg = "=\\s*[+-]?(\\d*)(\\.\\d*)?";
+		String likeReg = "(?i)like\\s*'([^']+)'";
+		String limitReg = "(?i)limit\\s*(\\d*),\\s*(\\d*)";
+		String reg = stringReg + "|" + numberReg;
+		
+		logger.info("fq : {}", FQ);
+		logger.info("dq : {}", DQ);
+		
+		String Fdq = FQ.replaceAll("\\?", "");
+		Fdq = Fdq.replaceAll(limitReg, "limit , ");
+		String Ddq = DQ.replaceAll(reg, "=");
+		Ddq = Ddq.replaceAll(likeReg, "like ");
+		Ddq = Ddq.replaceAll(limitReg, "limit , ");
+//		String DDQ = DQ.replaceAll(stringReg, "=''");
+//		DDQ = DDQ.replaceAll(numberReg, "=");
+//		DDQ = DDQ.replaceAll(likeReg, "like ''");
+		
+		logger.info("fdq : {}", Fdq);
+		logger.info("ddq : {}", Ddq);
+		
+		byte[] FdqBytes = Fdq.getBytes();
+		byte[] DdqBytes = Ddq.getBytes();
+		byte[] result = new byte[FdqBytes.length]; 
+		boolean check = false;
+		
+		for (int i = 0;i < FdqBytes.length;++i) {
+			result[i] = (byte)(FdqBytes[i] ^ DdqBytes[i]);
+			if (result[i] != 0x00) {
+				check = true;
+				break;
+			}
+		}
+		
+		//	check true -> abnormal sql
+		//	check false -> normal sql
+		//	return false -> abnormal sql
+		//	return true -> normal sql
+		if (check) {
+			logger.info("abnormal sql");
+			return false;
+		}
+		logger.info("normal sql");
+		return true;
 	}
 
 }
